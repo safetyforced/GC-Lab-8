361de4cea685f35751dbdb0fbb7b966262367dde
Alex Marks <marks.alexanderj@gmail.com>
2016-04-06 21:33:23 -0400

    comments added, typo fixed, pleasantness added

--- src/battingAverages/BattingAverages.java
+++ src/battingAverages/BattingAverages.java
@@ -1,4 +1,8 @@
 package battingAverages;
+/**
+ * @author Alex Marks
+ * Batting Average Calculator
+ */
 
 import java.util.Scanner;
 
@@ -12,24 +16,23 @@ public class BattingAverages {
 
 		while (userConsent) {
 
-			System.out.print("Enter number of times and bat: ");
+			System.out.print("Enter number of times at bat: ");
 			int onPlate = input.nextInt();
-			double[] basesEarned = new double[onPlate];
+			double[] basesEarned = new double[onPlate];  //create array sized proportionally to the number of hits
 
-			for (int i = 0; i < onPlate; i++) {
+			for (int i = 0; i < onPlate; i++) {  //ask user for each instance at bat. (Room for improvement here?  string split and parse?)
 				System.out.print("Result for at-bat: ");
 				basesEarned[i] = input.nextDouble();
 				if (basesEarned[i] < 0 || basesEarned[i] > 4) {
-					System.out.println("Dude, learn baseball.");
+					System.out.println("Dude, learn baseball.");  //insult user
 					i--;
 				}
-					
+
 			}
-			
+
 			double batAvg = bAvg(basesEarned);
 			double slugAvg = sAvg(basesEarned);
 
-			
 			System.out.println("Batting Average: " + batAvg);
 			System.out.println("Slugging Percentage: " + slugAvg);
 
@@ -39,31 +42,32 @@ public class BattingAverages {
 			if (cont.equalsIgnoreCase("n")) {
 				userConsent = false;
 			}
-			
+
 		}
 		input.close();
-		System.out.println("Bye.");
+		System.out.println("You're beautiful.");  //compliment user
 	}
 
-	public static double bAvg(double[] bases) {
+	public static double bAvg(double[] bases) {  //calculate batting average
 		double hits = 0.0;
 		for (int i = 0; i < bases.length; i++) {
 			if (bases[i] > 0) {
-				hits++;
+				hits++;  //increment variable for each instance a hit was made
 			}
 		}
 		double average = (hits / bases.length);
 		return average;
 
 	}
-	public static double sAvg(double[] bases) {
+
+	public static double sAvg(double[] bases) {  //calculate slugging percentage
 		double slug = 0.0;
 		for (int i = 0; i < bases.length; i++) {
-			slug += bases[i];
+			slug += bases[i];  //add total number of bases taken
 		}
 		double average = (slug / bases.length);
 		return average;
 
 	}
-	
+
 }
